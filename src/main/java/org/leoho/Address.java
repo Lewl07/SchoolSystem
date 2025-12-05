77f36ea483089051d24f74cd88ee9a11a718e212
Leo <6375071@edu.vaniercollege.qc.ca>
2025-12-05 08:51:34 -0500

    Create the Address class

---
+++ src/main/java/org/leoho/Address.java
@@ -0,0 +1,9 @@
+package org.leoho;
+
+public class Address {
+    private int streetNo;
+    private String street;
+    private String city;
+    private Province Province;
+    private String zipCode;
+}
