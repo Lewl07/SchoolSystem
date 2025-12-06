c05e9cf877bfdbf4f7a5fa99e5adeb6f492ffaa8
Leo <6375071@edu.vaniercollege.qc.ca>
2025-12-06 03:08:45 -0500

    Finish Department class code for helper method isDepartmentNameValid and constructor

--- src/main/java/org/leoho/Department.java
+++ src/main/java/org/leoho/Department.java
@@ -1,4 +1,47 @@
 package org.leoho;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@EqualsAndHashCode
+@Getter
 public class Department {
+    private final String departmentId;
+    @Setter private String departmentName;
+    private static int nextId;
+
+    /**
+     * Checks if a department name is valid or not.
+     * the name should only contain letters or space
+     * @param departmentName the department name
+     * @return whether the department name is valid.
+     */
+    public static boolean isDepartmentNameValid(String departmentName) {
+        if (departmentName == null || departmentName.isEmpty()) {
+            return false;
+        }
+
+        for (int i = 0; i < departmentName.length(); i++) {
+            char c = departmentName.charAt(i);
+
+            if (!Character.isLetter(c) && !Character.isWhitespace(c)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public Department(String departmentName) {
+        if (isDepartmentNameValid(departmentName)) {
+            this.departmentId = String.format("D%02d", nextId++);
+            this.departmentName = departmentName;
+        } else {
+            this.departmentId = null;
+            this.departmentName = null;
+        }
+    }
 }
