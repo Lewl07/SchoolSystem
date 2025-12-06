47f34248214280ba2241507eabf7215d618735c5
Leo <lewlvn1@gmail.com>
2025-12-06 00:50:06 -0500

    Add all argument Constructor with method isPostalCodeValid() called

--- src/main/java/org/leoho/Address.java
+++ src/main/java/org/leoho/Address.java
@@ -1,10 +1,19 @@
 package org.leoho;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@EqualsAndHashCode
+@Getter
+@Setter
 public class Address {
     private int streetNo;
     private String street;
     private String city;
-    private Province Province;
+    private Province province;
     private String postalCode;
 
     /**
@@ -32,6 +41,22 @@ public class Address {
         return true;
     }
 
+    // If the postal code is invalid, the entire address is reverted to null.
+    public Address(int streetNo, String street, String city, Province province, String postalCode) {
+        if (isPostalCodeValid(postalCode)) {
+            this.postalCode = postalCode.toUpperCase();
+            this.streetNo = streetNo;
+            this.street = street;
+            this.city = city;
+            this.province = province;
+        } else {
+            this.postalCode = null;
+            this.streetNo = -1;
+            this.street = null;
+            this.city = null;
+            this.province = null;
+        }
+    }
 
     public enum Province {
         ON, QC, AB, BC, MB, SK, NB, NL, NS, PE
