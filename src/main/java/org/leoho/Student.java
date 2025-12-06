87920264a39492f025c56e918a425a0e4b3dbe9e
Leo <6375071@edu.vaniercollege.qc.ca>
2025-12-06 03:12:56 -0500

    Create the Student class

---
+++ src/main/java/org/leoho/Student.java
@@ -0,0 +1,15 @@
+package org.leoho;
+
+public class Student {
+    private String studentId;
+    private String studentName;
+    private Gender gender;
+    private Address address;
+    private Department department;
+    private ArrayList<Course> registeredCourses;
+    private static int nextId;
+
+    public enum Gender {
+        MALE, FEMALE
+    }
+}
