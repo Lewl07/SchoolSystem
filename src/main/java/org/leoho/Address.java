7d2e058eaef9a19bdb8bdd851a616d9fa6373bf7
Leo <lewlvn1@gmail.com>
2025-12-06 00:09:34 -0500

    Add helper method isPosterCodeValid and enum Province

--- src/main/java/org/leoho/Address.java
+++ src/main/java/org/leoho/Address.java
@@ -5,5 +5,35 @@ public class Address {
     private String street;
     private String city;
     private Province Province;
-    private String zipCode;
+    private String postalCode;
+
+    /**
+     * Checks if a postcode is valid or not.
+     * @param postalCode the postal code
+     * @return whether the postal code is valid or not.
+     */
+    public static boolean isPostalCodeValid(String postalCode) {
+        if (postalCode.length() != 6) {
+            return false;
+        }
+
+        for (int i = 0; i < postalCode.length() - 1; i++) {
+            char c = postalCode.charAt(i);
+
+            if (i % 2 == 0 && !Character.isLetter(c)) {
+                return false;
+            }
+
+            if (i % 2 == 1 && !Character.isDigit(c)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
+    public enum Province {
+        ON, QC, AB, BC, MB, SK, NB, NL, NS, PE
+    }
 }
