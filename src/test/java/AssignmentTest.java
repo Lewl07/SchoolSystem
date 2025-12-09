1766b17ef93c241e56dda32a3d8cad46d862ae7c
Leo <6375071@edu.vaniercollege.qc.ca>
2025-12-08 21:06:26 -0500

    Begin coding methods for Assignment class

--- src/test/java/AssignmentTest.java
+++ src/test/java/AssignmentTest.java
@@ -1,2 +1,10 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class AssignmentTest {
+
+    @Test
+    @DisplayName("[10, 20, 30] -> 20")
+    void calcAssignmentAvg() {
+    }
 }
