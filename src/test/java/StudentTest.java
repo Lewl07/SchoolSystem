e5e77ff1b51d6ad4facdffe06ef580a7204769e3
Leo <6375071@edu.vaniercollege.qc.ca>
2025-12-14 02:35:01 -0500

    Test methods of the Student class in StudentTest

---
+++ src/test/java/StudentTest.java
@@ -0,0 +1,95 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.leoho.Address;
+import org.leoho.Course;
+import org.leoho.Department;
+import org.leoho.Student;
+
+public class StudentTest {
+    // 1. registerCourse()
+
+    @Test
+    @DisplayName("Student that DOESN'T have the course registered -> true")
+    void registerCourseTest1() {
+        Address address = new Address(1, "Main", "MTL",
+                Address.Province.QC, "a1b2c3");
+
+        Department department = new Department("CompSci");
+
+        Course introToProg = new Course("Prog", 3.0, department);
+
+        Student s1 = new Student("Leo", Student.Gender.MALE,
+                address, department);
+
+        boolean expected = true;
+        boolean actual = s1.registerCourse(introToProg);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("Student registering for a course he ALREADY registered -> false")
+    void registerCourseTest2() {
+        Address address = new Address(1, "Main", "MTL",
+                Address.Province.QC, "a1b2c3");
+
+        Department department = new Department("CompSci");
+
+        Course introToProg = new Course("Prog", 3.0, department);
+
+        Student s1 = new Student("Leo", Student.Gender.MALE,
+                address, department);
+
+        s1.registerCourse(introToProg);     // s1 has already registered for the course
+
+        boolean expected = false;
+        boolean actual = s1.registerCourse(introToProg);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    //2. dropCourse()
+
+    @Test
+    @DisplayName("Student drops a course that he is currently registered for -> true")
+    void dropCourseTest1() {
+        Address address = new Address(1, "Main", "MTL",
+                Address.Province.QC, "a1b2c3");
+
+        Department department = new Department("CompSci");
+
+        Course introToProg = new Course("Prog", 3.0, department);
+
+        Student s1 = new Student("Leo", Student.Gender.MALE,
+                address, department);
+
+        s1.registerCourse(introToProg);     // s1 is currently registered in a course that he wants to drop
+
+        boolean expected = true;
+        boolean actual = s1.dropCourse(introToProg);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("Student drops a course that he has YET to register for -> true")
+    void dropCourseTest2() {
+        Address address = new Address(1, "Main", "MTL",
+                Address.Province.QC, "a1b2c3");
+
+        Department department = new Department("CompSci");
+
+        Course introToProg = new Course("Prog", 3.0, department);
+
+        Student s1 = new Student("Leo", Student.Gender.MALE,
+                address, department);
+
+//      s1.registerCourse(introToProg);     // s1 wasn't in the course in the first place
+
+        boolean expected = false;
+        boolean actual = s1.dropCourse(introToProg);
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
