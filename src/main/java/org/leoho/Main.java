a4d71c8f2ea51d998b09991f1bfbfe123cb9dfe5
Leo <6375071@edu.vaniercollege.qc.ca>
2025-12-14 23:37:39 -0500

    Testing displayScores() method in the Main class

--- src/main/java/org/leoho/Main.java
+++ src/main/java/org/leoho/Main.java
@@ -1,17 +1,29 @@
 package org.leoho;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Department department = new Department("CompSci");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Course introToProg = new Course("Introduction to Programming", 3.0, department);
+
+        Student s1 = new Student("Leo", Student.Gender.MALE,
+                null, department);
+        Student s2 = new Student("Mike", Student.Gender.MALE,
+                null, department);
+        Student s3 = new Student("Bob", Student.Gender.MALE,
+                null, department);
+
+        introToProg.addAssignment("Assignment01", 20, 100);
+        introToProg.addAssignment("Exam01", 80, 100);
+
+        s1.getRegisteredCourses().add(introToProg);
+        s2.getRegisteredCourses().add(introToProg);
+        s3.getRegisteredCourses().add(introToProg);
+
+        introToProg.registerStudent(s1);
+        introToProg.registerStudent(s2);
+        introToProg.registerStudent(s3);
+
+        introToProg.displayScores();
     }
 }
